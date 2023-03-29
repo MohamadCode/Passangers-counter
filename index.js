d0997758813d612bb058157649132758cfaff17e
MohamadCode <m-munla2004@hotmail.com>
2023-03-29 15:23:00 +0300

    js

--- index.js
+++ index.js
@@ -4,18 +4,9 @@
    
 
 
-let bonuspoints = 50
+function increment() {
+    console.log("42")
+    
+}
 
-
-bonuspoints = bonuspoints + 100 
-
-console.log(bonuspoints)
-
-bonuspoints = bonuspoints - 25
-
-console.log(bonuspoints)
-
-bonuspoints = bonuspoints + 70
-console.log(bonuspoints)
-
-console.log(bonuspoints)
+increment
