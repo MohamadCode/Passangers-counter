3d52f32dd3f98feef9bdc3e3da5afb7428e97cdf
MohamadCode <m-munla2004@hotmail.com>
2023-04-05 14:37:47 +0300

    count-0

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ let count = 0
 
 function increment() {
     count += 1
-    counteEl.innerText = count
+    counteEl.textContent = count 
+    
+
 }
 
 
 
 function save() {
-    let entry = count +g " - "
-    saveEl.innerText += entry
-
+    let entry = count + " - "
+    saveEl.textContent += entry 
+    counteEl.textContent = 0
+    count = 0
     console.log(count)
 }
 
