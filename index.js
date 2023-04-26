8b2895769f5be4fb49245e57ed5b9305e602ff53
MohamadCode <m-munla2004@hotmail.com>
2023-04-26 06:04:06 +0300

    done

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-
+let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
-let counteEl = document.getElementById("count-el")
 let count = 0
 
+
 function increment() {
-    count += 1
-    counteEl.textContent = count 
-
+    count+= 1
+    countEl.textContent = count
 }
-
-
-
 
 
 function save() {
     let entry = count + " - "
-    saveEl.textContent += entry 
-    counteEl.textContent = 0
+    saveEl.textContent += entry
+    countEl.textContent = 0
     count = 0
-    console.log(count)
 }
 
 
