2a85c8e6500d345b971631179ddb052872a3dcd1
MohamadCode <m-munla2004@hotmail.com>
2023-10-12 02:06:34 +0300

    functions

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-let count = 1
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+
+let count = 0
 
 console.log(count)
 
 
 function increment() {
-    
+
+    count = count + 1
+    countEl.textContent = count
+    console.log(count)
 }
